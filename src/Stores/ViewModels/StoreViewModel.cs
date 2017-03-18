e97740a1b9098ab099aace66536ddcd39bc563aa
Glendon Cheney <glendon.cheney@gmail.com>
2017-03-18 18:51:24 -0500

    Add ViewModel to handle direct interaction with view

---
+++ src/Stores/ViewModels/StoreViewModel.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Stores.ViewModels
+{
+    public class StoreViewModel
+    {
+        [Required]
+        [Display(Name ="Store Number")]
+        public int StoreNumber { get; set; }
+
+        [Display(Name ="Store Name")]
+        public string StoreName { get; set; }
+
+        [Display(Name ="Store Manager Name")]
+        public string StoreManagerName { get; set; }
+
+        [Display(Name ="Opening Time")]
+        public string OpeningTime { get; set; }
+
+        [Display(Name ="Closing Time")]
+        public string ClosingTime { get; set; }
+    }
+}
