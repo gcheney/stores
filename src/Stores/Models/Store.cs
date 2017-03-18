1f8ac6b73ac7a43a6826e093916107800ddc5888
Glendon Cheney <glendon.cheney@gmail.com>
2017-03-18 10:57:30 -0500

    Add Store model

---
+++ src/Stores/Models/Store.cs
@@ -0,0 +1,11 @@
+namespace Stores.Models
+{
+    public class Store
+    {
+        public int StoreNumber { get; set; }
+        public string StoreName { get; set; }
+        public string StoreManagerName { get; set; }
+        public string OpeningTime { get; set; }
+        public string ClosingTime { get; set; }
+    }
+}
