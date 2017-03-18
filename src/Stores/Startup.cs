0364ee8cea930335c0eb9113cbd49596891969eb
Glendon Cheney <glendon.cheney@gmail.com>
2017-03-18 12:12:32 -0500

    Register repo and file manager services with DI container

--- src/Stores/Startup.cs
+++ src/Stores/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Stores.Services;
 
 namespace Stores
 {
@@ -33,6 +34,9 @@ namespace Stores
             
             // use lowercase routes
             services.AddRouting(options => options.LowercaseUrls = true);
+
+            services.AddScoped<IStoreRepository, StoreRepository>();
+            services.AddTransient<ICsvFileManager, CsvFileManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
