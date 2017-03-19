8316a204d2dc9e0c62c0523d1a140aee0fbfae92
Glendon Cheney <glendon.cheney@gmail.com>
2017-03-19 10:41:04 -0500

    Implement api method for adding new stores

--- src/Stores/Controllers/Api/StoreController.cs
+++ src/Stores/Controllers/Api/StoreController.cs
@@ -1,17 +1,56 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Stores.Models;
+using Stores.ViewModels;
+using Stores.Services;
+using AutoMapper;
 
 namespace Stores.Controllers.Api
 {
     public class StoreController : Controller
     {
-        [HttpPost("api/store/add")]
-        public IActionResult AddStore()
+        private IStoreRepository _storeRepo;
+        private ILogger<StoreController> _logger;
+
+        public StoreController(IStoreRepository storeRepo, ILoggerFactory loggerFactory)
         {
-            return Ok();
+            _storeRepo = storeRepo;
+            _logger = loggerFactory.CreateLogger<StoreController>();
+        } 
+        
+        [HttpPost("api/store/add")]
+        public IActionResult AddStore([FromBody] StoreViewModel storeViewModel)
+        {
+            if (storeViewModel == null)
+            {
+                _logger.LogInformation("No data provided");
+                return BadRequest();
+            }
+
+            if (storeViewModel.StoreNumber == 0)
+            {
+                ModelState.AddModelError("StoreNumber", "Invalid or null store number supplied");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                _logger.LogInformation("Invalid store data was provided");
+                return BadRequest(ModelState);
+            }
+
+            var store = Mapper.Map<Store>(storeViewModel);
+
+            // store was successfully created
+            if (_storeRepo.AddStore(store))
+            {
+                return CreatedAtRoute("Detail", new {
+                    storeNumber = store.StoreNumber
+                }, store);
+            }
+
+            return StatusCode(409, new {
+                ErrorMessage = "A resource with the specified StoreNumber already exist"
+            });  
         }
 
         [HttpPost("api/store/{storeNumber}")]
