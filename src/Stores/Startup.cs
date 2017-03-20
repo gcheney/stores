dd7666f8589d9ae2e9170b271536753fa9f0d546
Glendon Cheney <glendon.cheney@gmail.com>
2017-03-20 12:28:26 -0500

    Add CORS policy

--- src/Stores/Startup.cs
+++ src/Stores/Startup.cs
@@ -41,6 +41,8 @@ namespace Stores
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseCors(options => options.AllowAnyOrigin());
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
